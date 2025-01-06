7dc479f0d3523656e106af6a1d3212700c4b221b
saharsh vashishtha <saharsh.vashishtha@gmail.com>
2025-01-06 23:10:08 +0530

    changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -49,7 +49,7 @@ app.use(session({
 }));
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://auto-ui-wbyo-dvpjkkt85-saharsh-vahsishthas-projects.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization','WithCredentials'],
